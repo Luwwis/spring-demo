f0106e5b44283872e9d91cce3556f866bfb989c7
leilu <leilu@didichuxing.com>
2019-08-13 11:28:44 +0800

    dateutil

--- spring-demo-common/src/main/java/com/xiaoju/leilu/spring/demo/common/util/DateUtils.java
+++ spring-demo-common/src/main/java/com/xiaoju/leilu/spring/demo/common/util/DateUtils.java
@@ -1,7 +1,28 @@
 package com.xiaoju.leilu.spring.demo.common.util;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by leilu on 2019/8/13.
  */
-public class DateUtils {
+public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
+    public static String dateToString(Date date,String format){
+        SimpleDateFormat sdf=new SimpleDateFormat(format);
+        return sdf.format(date);
+    }
+    public static Date stringToDate(String dateStr,String format){
+        SimpleDateFormat sdf=new SimpleDateFormat(format);
+        try {
+            return sdf.parse(dateStr);
+        } catch (ParseException e) {
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        String dateStr="20180101";
+        Date date=DateUtils.stringToDate(dateStr,"yyyyMMdd");
+        System.out.println();
+    }
 }
